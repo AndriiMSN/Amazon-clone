2ad4aa1b8006a0ea7f401aa0173cb09106ebaa56
Andrii Mosin <andrewmosin8@gmail.com>
2020-09-27 21:02:43 +0300

    Updates

---
+++ precache-manifest.7a7da35cffbf37c154ed96babb6e454e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b248d72ed9ac55f7d4e521bb87d9d06a",
+    "url": "/Amazon-clone/index.html"
+  },
+  {
+    "revision": "359e7037b456156f5394",
+    "url": "/Amazon-clone/static/css/main.338a9d88.chunk.css"
+  },
+  {
+    "revision": "e02db967bbd34f2eaa41",
+    "url": "/Amazon-clone/static/js/2.af066920.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/Amazon-clone/static/js/2.af066920.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "359e7037b456156f5394",
+    "url": "/Amazon-clone/static/js/main.83d503f6.chunk.js"
+  },
+  {
+    "revision": "b4ca44839ab564cdfff3",
+    "url": "/Amazon-clone/static/js/runtime-main.a692d69b.js"
+  }
+]);
